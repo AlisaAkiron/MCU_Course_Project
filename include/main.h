8cdbe6110394bae5d7a8f2228ee1477744bed0c0
Liam Sho <conan_creeper@outlook.com>
2021-12-21 03:13:38 +0800

    fix.修复头文件错误

--- include/main.h
+++ include/main.h
@@ -5,7 +5,6 @@
 #ifndef MCU_COURSE_PROJECT_MAIN_H
 #define MCU_COURSE_PROJECT_MAIN_H
 
-#endif //MCU_COURSE_PROJECT_MAIN_H
 
 void pin_setup();
 void wifi_setup();
@@ -19,3 +18,6 @@ void onMqttSubscribe(uint16_t packetId, uint8_t qos);
 void onMqttUnsubscribe(uint16_t packetId);
 void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
 void onMqttPublish(uint16_t packetId);
+
+
+#endif //MCU_COURSE_PROJECT_MAIN_H
