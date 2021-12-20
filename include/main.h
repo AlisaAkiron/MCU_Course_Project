8b3d5e3b2b0f83e79bf846b82022776b115f9b77
Liam Sho <conan_creeper@outlook.com>
2021-12-21 03:13:01 +0800

    feat.添加 Mqtt 逻辑

--- include/main.h
+++ include/main.h
@@ -9,3 +9,13 @@
 
 void pin_setup();
 void wifi_setup();
+
+void mqtt_setup();
+void mqtt_connect();
+
+void onMqttConnect(bool sessionPresent);
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
+void onMqttSubscribe(uint16_t packetId, uint8_t qos);
+void onMqttUnsubscribe(uint16_t packetId);
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+void onMqttPublish(uint16_t packetId);
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
 #include <ESPAsync_WiFiManager.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
 
 #include "main.h"
-
-#define BAUD_RATE 9600
+#include "configuration.h"
 
 AsyncWebServer webServer(80);
 DNSServer dnsServer;
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
 
 void setup() {
     Serial.begin(BAUD_RATE);
@@ -17,6 +21,9 @@ void setup() {
 
     pin_setup();
     wifi_setup();
+    mqtt_setup();
+
+    mqtt_connect();
 }
 
 void loop() {
@@ -60,4 +67,82 @@ void wifi_setup() {
         Serial.print("[WIFI] Failed to connect to WiFi - ");
         Serial.println(wifiManager.getStatus(WiFiClass::status()));
     }
+}
+
+
+/**
+ * @brief MQTT setup
+ */
+void mqtt_setup() {
+    Serial.println("[MQTT] Setup MQTT");
+    mqttClient.onConnect(onMqttConnect);
+    mqttClient.onDisconnect(onMqttDisconnect);
+    mqttClient.onSubscribe(onMqttSubscribe);
+    mqttClient.onUnsubscribe(onMqttUnsubscribe);
+    mqttClient.onMessage(onMqttMessage);
+    mqttClient.onPublish(onMqttPublish);
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    Serial.println("[MQTT] MQTT setup finished");
+}
+
+void mqtt_connect() {
+    Serial.println("[MQTT] Connecting to MQTT");
+    mqttClient.connect();
+}
+void onMqttConnect(bool sessionPresent) {
+    Serial.println("[MQTT] Connected to MQTT successfully");
+    Serial.print("[MQTT] Session present: ");
+    Serial.println(sessionPresent);
+    uint16_t packetIdSub = mqttClient.subscribe(MQTT_TOPIC, MQTT_TOPIC_QOS);
+    Serial.print("[MQTT] Subscribing at QoS " + String(MQTT_TOPIC_QOS) + ", packetId: ");
+    Serial.println(packetIdSub);
+}
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+    Serial.println("[MQTT] Disconnected from MQTT.");
+
+    if (reason == AsyncMqttClientDisconnectReason::TLS_BAD_FINGERPRINT) {
+        Serial.println("[MQTT] Bad server fingerprint.");
+    }
+
+    if (WiFi.isConnected()) {
+        Serial.println("[MQTT] Try to reconnect to MQTT in 5 seconds...");
+        mqttReconnectTimer.once(MQTT_RECONNECT_TIMEOUT, mqtt_connect);
+    }
+}
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+    Serial.println("[MQTT] Subscribe acknowledged.");
+    Serial.print("    - packetId: ");
+    Serial.println(packetId);
+    Serial.print("    - qos: ");
+    Serial.println(qos);
+}
+void onMqttUnsubscribe(uint16_t packetId) {
+    Serial.println("[MQTT] Unsubscribe acknowledged.");
+    Serial.print("    - packetId: ");
+    Serial.println(packetId);
+}
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+    Serial.println("[MQTT] Publish received. Message info:");
+    Serial.print("    - topic: ");
+    Serial.println(topic);
+    Serial.print("    - qos: ");
+    Serial.println(properties.qos);
+    Serial.print("    - dup: ");
+    Serial.println(properties.dup);
+    Serial.print("    - retain: ");
+    Serial.println(properties.retain);
+    Serial.print("    - len: ");
+    Serial.println(len);
+    Serial.print("    - index: ");
+    Serial.println(index);
+    Serial.print("    - total: ");
+    Serial.println(total);
+    Serial.println("    - payload: ");
+    Serial.println(payload);
+    Serial.println("[MQTT] Message info printing finished");
+}
+void onMqttPublish(uint16_t packetId) {
+    Serial.println("[MQTT] Publish acknowledged.");
+    Serial.print("    - packetId: ");
+    Serial.println(packetId);
 }
