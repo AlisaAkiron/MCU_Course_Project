3b9274311aef22f364e62c448402e76fd5a247c5
Liam Sho <conan_creeper@outlook.com>
2021-12-21 02:59:20 +0800

    doc.修改串口日志输出文本

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ void setup() {
     while (!Serial) {
         delay(100);
     }
-    Serial.println("Serial started");
+    Serial.println("[SERIAL] Serial started with baud rate: " + String(BAUD_RATE));
 
     pin_setup();
     wifi_setup();
@@ -26,38 +26,38 @@ void loop() {
  * @brief Pin setup
  */
 void pin_setup() {
-    Serial.println("Start pin setup process");
+    Serial.println("[PIN] Start pin setup process");
 
     // Builtin LED
     pinMode(LED_BUILTIN, OUTPUT);
-    Serial.println("Setup pin: LED_BUILTIN as OUTPUT");
+    Serial.println("[PIN] Setup pin: LED_BUILTIN as OUTPUT");
     // Wifi Reset Pin
     pinMode(GPIO_NUM_16, INPUT);
-    Serial.println("Setup pin: GPIO_NUM_16 as INPUT");
+    Serial.println("[PIN] Setup pin: GPIO_NUM_16 as INPUT");
 
-    Serial.println("Pin setup process finished");
+    Serial.println("[PIN] Pin setup process finished");
 }
 
 /**
  * @brief WiFi setup
  */
 void wifi_setup() {
-    Serial.println("Connecting to WiFi");
-    Serial.print("WiFiManager: ");
+    Serial.println("[WIFI] Connecting to WiFi");
+    Serial.print("[WIFI] WiFiManager: ");
     Serial.println(ESP_ASYNC_WIFIMANAGER_VERSION);
     ESPAsync_WiFiManager wifiManager = ESPAsync_WiFiManager(&webServer, &dnsServer, "NodeMCU-ESP32-S");
     if (digitalRead(GPIO_NUM_16) == HIGH) {
-        Serial.println("Wifi Reset Pin is HIGH, reset WiFi config");
+        Serial.println("[WIFI] Wifi Reset Pin is HIGH, reset WiFi config");
         wifiManager.resetSettings();
     }
     wifiManager.autoConnect("NodeMCU-ESP32-S");
     if (WiFiClass::status() == WL_CONNECTED) {
-        Serial.println("Connected to WiFi");
-        Serial.println("SSID: " + WiFi.SSID());
-        Serial.println("IP Address: " + WiFi.localIP().toString());
+        Serial.println("[WIFI] Connected to WiFi");
+        Serial.println("    - SSID: " + WiFi.SSID());
+        Serial.println("    - IP Address: " + WiFi.localIP().toString());
     }
     else {
-        Serial.print("Failed to connect to WiFi - ");
+        Serial.print("[WIFI] Failed to connect to WiFi - ");
         Serial.println(wifiManager.getStatus(WiFiClass::status()));
     }
 }
