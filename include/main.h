c4a4101e78a97a4655057749f70528b5b65ac2f9
Liam Sho <conan_creeper@outlook.com>
2021-12-21 02:56:58 +0800

    feat.添加 Wifi 连接配置

---
+++ include/main.h
@@ -0,0 +1,11 @@
+//
+// Created by Liam Sho on 2021/12/21.
+//
+
+#ifndef MCU_COURSE_PROJECT_MAIN_H
+#define MCU_COURSE_PROJECT_MAIN_H
+
+#endif //MCU_COURSE_PROJECT_MAIN_H
+
+void pin_setup();
+void wifi_setup();
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,63 @@
 #include <Arduino.h>
+#include <ESPAsync_WiFiManager.h>
+
+#include "main.h"
+
+#define BAUD_RATE 9600
+
+AsyncWebServer webServer(80);
+DNSServer dnsServer;
+
 void setup() {
-// write your initialization code here
+    Serial.begin(BAUD_RATE);
+    while (!Serial) {
+        delay(100);
+    }
+    Serial.println("Serial started");
+
+    pin_setup();
+    wifi_setup();
 }
 
 void loop() {
-// write your code here
+}
+
+/**
+ * @brief Pin setup
+ */
+void pin_setup() {
+    Serial.println("Start pin setup process");
+
+    // Builtin LED
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.println("Setup pin: LED_BUILTIN as OUTPUT");
+    // Wifi Reset Pin
+    pinMode(GPIO_NUM_16, INPUT);
+    Serial.println("Setup pin: GPIO_NUM_16 as INPUT");
+
+    Serial.println("Pin setup process finished");
+}
+
+/**
+ * @brief WiFi setup
+ */
+void wifi_setup() {
+    Serial.println("Connecting to WiFi");
+    Serial.print("WiFiManager: ");
+    Serial.println(ESP_ASYNC_WIFIMANAGER_VERSION);
+    ESPAsync_WiFiManager wifiManager = ESPAsync_WiFiManager(&webServer, &dnsServer, "NodeMCU-ESP32-S");
+    if (digitalRead(GPIO_NUM_16) == HIGH) {
+        Serial.println("Wifi Reset Pin is HIGH, reset WiFi config");
+        wifiManager.resetSettings();
+    }
+    wifiManager.autoConnect("NodeMCU-ESP32-S");
+    if (WiFiClass::status() == WL_CONNECTED) {
+        Serial.println("Connected to WiFi");
+        Serial.println("SSID: " + WiFi.SSID());
+        Serial.println("IP Address: " + WiFi.localIP().toString());
+    }
+    else {
+        Serial.print("Failed to connect to WiFi - ");
+        Serial.println(wifiManager.getStatus(WiFiClass::status()));
+    }
 }
