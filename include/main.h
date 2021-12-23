7193c027fcb88884caa580ab6dafe1cc1bd47dbb
Liam Sho <conan_creeper@outlook.com>
2021-12-23 21:25:56 +0800

    feat.完成显示时钟功能

--- include/main.h
+++ include/main.h
@@ -5,12 +5,16 @@
 #ifndef MCU_COURSE_PROJECT_MAIN_H
 #define MCU_COURSE_PROJECT_MAIN_H
 
+enum Mission {
+    no_mission,
+    clock_display_update
+};
 
 void pin_setup();
 void wifi_setup();
 void led_setup();
+void time_sync_setup();
 void mqtt_setup();
-
 void mqtt_connect();
 
 void onMqttConnect(bool sessionPresent);
@@ -23,5 +27,21 @@ void onMqttPublish(uint16_t packetId);
 void log(const String& message);
 
 void mqttMessageHandler(char* data);
+
+void led_mode_helper(const ArduinoJson6185_91::StaticJsonDocument<4096>& doc);
+
+void display_single_pixel(int16_t x, int16_t y, RgbColor color);
+void display_clock(int tz);
+
+void time_display_update_mission();
+void led_matrix_refresh();
+void led_matrix_init();
+
+time_t get_ntp_time();
+void sendNTPPacket(IPAddress &address);
+
+void set_digit_color(int16_t start_x, int16_t start_y, char ch);
+void set_digit_color(int16_t start_x, int16_t start_y, const char* group);
+
 
 #endif //MCU_COURSE_PROJECT_MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <AsyncMqttClient.h>
 #include <ArduinoJson.h>
 #include <NeoPixelBus.h>
+#include <TimeLib.h>
 
 #include "main.h"
 #include "configuration.h"
@@ -17,6 +18,18 @@ Ticker mqttReconnectTimer;
 NeoPixelBus<NeoRgbFeature, Neo800KbpsMethod> strip(NEO_PIXEL_COUNT, NEO_PIXEL_PIN);
 NeoTopology<ColumnMajorAlternatingLayout> matrix(NEO_PIXEL_WIDTH, NEO_PIXEL_HEIGHT);
 
+RgbColor colors[NEO_PIXEL_WIDTH][NEO_PIXEL_HEIGHT];
+RgbColor digitColor(255, 255, 255);
+const RgbColor blackColor(0, 0, 0);
+
+WiFiUDP Udp;
+unsigned int localPort = 8888;
+static const char ntpServerName[] = "ntp.ntsc.ac.cn";
+int timezone = 0;
+time_t prevDisplay = 0;
+
+Mission LoopMission = no_mission;
+
 void setup() {
     Serial.begin(BAUD_RATE);
     while (!Serial) {
@@ -28,11 +41,26 @@ void setup() {
     wifi_setup();
     mqtt_setup();
     led_setup();
-
     mqtt_connect();
+    delay(2000);
+    time_sync_setup();
 }
 
 void loop() {
+    if (timeStatus() != timeNotSet) {
+        if (now() != prevDisplay) {
+            prevDisplay = now();
+        }
+    }
+    switch (LoopMission) {
+        case clock_display_update:
+            time_display_update_mission();
+            delay(1000);
+            break;
+        case no_mission:
+        default:
+            break;
+    }
 }
 
 void pin_setup() {
@@ -80,11 +108,16 @@ void mqtt_setup() {
 }
 void led_setup() {
     Serial.println("[LED] Setup LED, display every LED in black");
-    for (int i = 0; i < NEO_PIXEL_COUNT; i++) {
-        strip.SetPixelColor(i, RgbColor(0, 0, 0));
-    }
-    strip.Show();
+    strip.Begin();
+    led_matrix_init();
     Serial.println("[LED] LED setup finished");
+}
+void time_sync_setup() {
+    log("[NTP] Setup time sync");
+    Udp.begin(localPort);
+    setSyncProvider(get_ntp_time);
+    setSyncInterval(3600);
+    log("[TIME] Time sync setup finished");
 }
 
 void mqtt_connect() {
@@ -157,10 +190,240 @@ void log(const String& message) {
 }
 
 void mqttMessageHandler(char* data) {
-    ArduinoJson6185_91::StaticJsonDocument<512> doc;
+    ArduinoJson6185_91::StaticJsonDocument<4096> doc;
     ArduinoJson6185_91::DeserializationError error = ArduinoJson6185_91::deserializeJson(doc, data);
     if (error) {
         log("[HANDLER] Json message deserialization failed: " + String(error.f_str()));
         return;
+    }
+
+    led_mode_helper(doc);
+}
+
+// LED matrix helpers
+void led_mode_helper(const ArduinoJson6185_91::StaticJsonDocument<4096>& doc) {
+    log("[HANDLER] Detecting led matrix mode");
+    const char* mode = doc["mode"];
+    if (mode == nullptr) {
+        log("[HANDLER] Mode not found, the \"mode\" field is missing");
+        return;
+    }
+    if (strcmp(mode, "single") == 0) {
+        log("[HANDLER] Single led mode detected");
+        int16_t single_x = doc["data"]["x"];
+        int16_t single_y = doc["data"]["y"];
+        int8_t single_r = doc["data"]["r"];
+        int8_t single_g = doc["data"]["g"];
+        int8_t single_b = doc["data"]["b"];
+        display_single_pixel(single_x, single_y, RgbColor(single_r, single_g, single_b));
+    }
+    else if (strcmp(mode, "clock") == 0) {
+        int clock_timezone_offset = doc["data"]["tz"];
+        display_clock(clock_timezone_offset);
+    }
+    else {
+        log("[HANDLER] Unknown mode detected");
+    }
+}
+void display_single_pixel(int16_t x, int16_t y, RgbColor color) {
+    colors[x][y] = color;
+    strip.SetPixelColor(matrix.Map(x, y), colors[x][y]);
+    strip.Show();
+}
+void display_clock(int tz) {
+    // Set timezone and fetch time from NTP
+    timezone = tz;
+    time_sync_setup();
+
+    // Clean LED matrix
+    led_matrix_init();
+
+    // Display clock dot
+    display_single_pixel(11, 2, digitColor);
+    display_single_pixel(11, 4, digitColor);
+    display_single_pixel(21, 2, digitColor);
+    display_single_pixel(21, 4, digitColor);
+
+    // Setup loop mission
+    LoopMission = Mission::clock_display_update;
+}
+
+/**********************/
+/*       Mission      */
+/**********************/
+
+// Loop mission
+void time_display_update_mission() {
+    String h_str = String(hour());
+    String m_str = String(minute());
+    String s_str = String(second());
+    if (h_str.length() == 1) {
+        h_str = "0" + h_str;
+    }
+    if (m_str.length() == 1) {
+        m_str = "0" + m_str;
+    }
+    if (s_str.length() == 1) {
+        s_str = "0" + s_str;
+    }
+    char h[2] = {h_str[0], h_str[1]};
+    char m[2] = {m_str[0], m_str[1]};
+    char s[2] = {s_str[0], s_str[1]};
+
+    set_digit_color(3, 1, h[0]);
+    set_digit_color(7, 1, h[1]);
+    set_digit_color(13, 1, m[0]);
+    set_digit_color(17, 1, m[1]);
+    set_digit_color(23, 1, s[0]);
+    set_digit_color(27, 1, s[1]);
+
+    led_matrix_refresh();
+}
+
+// Basic mission
+void led_matrix_refresh() {
+    for (int16_t x = 0; x < NEO_PIXEL_WIDTH; x++) {
+        for (int16_t y = 0; y < NEO_PIXEL_HEIGHT; y++) {
+            strip.SetPixelColor(matrix.Map(x, y), colors[x][y]);
+        }
+    }
+    strip.Show();
+}
+void led_matrix_init() {
+    for (auto & x : colors) {
+        for (auto & y : x) {
+            y = blackColor;
+        }
+    }
+    strip.Show();
+}
+
+/**********************/
+/*      Utilities     */
+/**********************/
+
+// NTP
+const int NTP_PACKET_SIZE = 48;
+byte packetBuffer[NTP_PACKET_SIZE];
+
+time_t get_ntp_time() {
+    IPAddress ntpServerIP;
+
+    while (Udp.parsePacket() > 0) ;
+    log("[NTP] Transmit NTP Request");
+    WiFiClass::hostByName(ntpServerName, ntpServerIP);
+    log("[NTP] Use NTP server " + String(ntpServerName) + " : " + String(ntpServerIP));
+    sendNTPPacket(ntpServerIP);
+    uint32_t beginWait = millis();
+    while (millis() - beginWait < 1500) {
+        int size = Udp.parsePacket();
+        if (size >= NTP_PACKET_SIZE) {
+            log("[NTP] Receive NTP Response");
+            Udp.read(packetBuffer, NTP_PACKET_SIZE);
+            unsigned long secsSince1900;
+            secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+            secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+            secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+            secsSince1900 |= (unsigned long)packetBuffer[43];
+            return secsSince1900 - 2208988800UL + timezone * 3600; // NOLINT(cppcoreguidelines-narrowing-conversions)
+        }
+    }
+    log("[NTP] NTP server not response :-(");
+    return 0;
+}
+void sendNTPPacket(IPAddress &address)
+{
+    // set all bytes in the buffer to 0
+    memset(packetBuffer, 0, NTP_PACKET_SIZE);
+    // Initialize values needed to form NTP request
+    // (see URL above for details on the packets)
+    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+    packetBuffer[1] = 0;     // Stratum, or type of clock
+    packetBuffer[2] = 6;     // Polling Interval
+    packetBuffer[3] = 0xEC;  // Peer Clock Precision
+    // 8 bytes of zero for Root Delay & Root Dispersion
+    packetBuffer[12] = 49;
+    packetBuffer[13] = 0x4E;
+    packetBuffer[14] = 49;
+    packetBuffer[15] = 52;
+    // all NTP fields have been given values, now
+    // you can send a packet requesting a timestamp:
+    Udp.beginPacket(address, 123); //NTP requests are to port 123
+    Udp.write(packetBuffer, NTP_PACKET_SIZE);
+    Udp.endPacket();
+}
+
+// Digit display set
+void set_digit_color(int16_t start_x, int16_t start_y, char ch) {
+    char group[16];
+    switch (ch) {
+        case '0':
+        {
+            String("XXXXOXXOXXOXXXX").toCharArray(group, 16);
+            Serial.println(group);
+            break;
+        }
+        case '1':
+        {
+            String("OXOOXOOXOOXOOXO").toCharArray(group, 16);
+            break;
+        }
+        case '2':
+        {
+            String("XXXOOXXXXXOOXXX").toCharArray(group, 16);
+            break;
+        }
+        case '3':
+        {
+            String("XXXOOXXXXOOXXXX").toCharArray(group, 16);
+            break;
+        }
+        case '4':
+        {
+            String("XOXXOXXXXOOXOOX").toCharArray(group, 16);
+            break;
+        }
+        case '5':
+        {
+            String("XXXXOOXXXOOXXXX").toCharArray(group, 16);
+            break;
+        }
+        case '6':
+        {
+            String("XXXXOOXXXXOXXXX").toCharArray(group, 16);
+            break;
+        }
+        case '7':
+        {
+            String("XXXOOXOOXOOXOOX").toCharArray(group, 16);
+            break;
+        }
+        case '8':
+        {
+            String("XXXXOXXXXXOXXXX").toCharArray(group, 16);
+            break;
+        }
+        case '9':
+        {
+            String("XXXXOXXXXOOXXXX").toCharArray(group, 16);
+            break;
+        }
+        default: {
+            String("XOXXOXOXOXOXXOX").toCharArray(group, 16);
+            break;
+        }
+    }
+    set_digit_color(start_x, start_y, group);
+}
+void set_digit_color(int16_t start_x, int16_t start_y, const char* group) {
+    for (int16_t y = start_y; y < start_y + 5; y++) {
+        for (int16_t x = start_x; x < start_x + 3; x++) {
+            if (group[3 * (y - start_y) + (x - start_x)] == 'X') {
+                colors[x][y] = digitColor;
+            }
+            else {
+                colors[x][y] = blackColor;
+            }
+        }
     }
 }
