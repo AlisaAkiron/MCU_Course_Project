80b0d394763c3103a25ed4a2c07a3929a85cd1f1
Liam Sho <conan_creeper@outlook.com>
2021-12-27 21:12:23 +0800

    feat.添加测试用 Logger

---
+++ client/ESPMonitor/Program.cs
@@ -0,0 +1,44 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using Serilog;
+
+var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var factory = new MqttFactory();
+var options = new MqttClientOptionsBuilder()
+    .WithClientId("dotnet-mqtt-logger")
+    .WithTcpServer("192.168.43.187")            // MQTT broker address
+    .WithCleanSession()
+    .Build();
+
+var mqttClient = factory.CreateMqttClient();
+
+logger.Information("Connecting to MQTT server...");
+await mqttClient.ConnectAsync(options, CancellationToken.None);
+while (mqttClient.IsConnected is false)
+{
+    logger.Information("Waiting for connection...");
+}
+
+logger.Information("Connected to MQTT server.");
+
+mqttClient.UseApplicationMessageReceivedHandler(eventArgs =>
+{
+    logger.Information($"{eventArgs.ApplicationMessage.ConvertPayloadToString()}");
+});
+
+await mqttClient.SubscribeAsync("esp/log");
+
+var key = new ConsoleKeyInfo();
+
+while (key != new ConsoleKeyInfo('Q', ConsoleKey.Q, false, false, true))
+{
+    key = Console.ReadKey();
+}
+
+logger.Information("Exiting...");
+await mqttClient.DisconnectAsync();
+logger.Information("Exited.");
